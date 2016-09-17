dd5c7cded93ce666b2ff2858756a31d66dcb56a6
Agoston Szepessy <agszepp@gmail.com>
2016-09-17 19:34:06 -0400

    Added google play services to gradle

--- app/src/main/java/western/alarm_app/MainActivity.java
+++ app/src/main/java/western/alarm_app/MainActivity.java
@@ -3,6 +3,8 @@ package western.alarm_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity
 {
 
