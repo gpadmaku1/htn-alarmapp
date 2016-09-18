dbf4d162f9e4d826b4b3d1685ef14e71823510ae
gpadmaku1 <gpadmaku@gmail.com>
2016-09-18 00:52:50 -0400

    Removed content_main.xml and cleaned up java code

--- app/src/main/java/western/alarm_app/MainActivity.java
+++ app/src/main/java/western/alarm_app/MainActivity.java
@@ -3,6 +3,8 @@ package western.alarm_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
     }
 }
